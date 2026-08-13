93d064247a3ccbefee1a41ea47b00baa51b17174
Enes Demir <123deli4567@gmail.com>
2026-08-13 22:56:35 +0300

    trying mid lr

--- colab/run_experiment_colab.py
+++ colab/run_experiment_colab.py
@@ -15,7 +15,7 @@
 # GatedRepoError regardless of the token being valid.
 
 # %%
-CONFIG_PATH = "configs/experiments/exp008h_native_berturk_mse_recipe_30pct_even_lower_lr.yaml"  # <- only thing to change per run
+CONFIG_PATH = "configs/experiments/exp008i_native_berturk_mse_recipe_30pct_mid_lr.yaml"  # <- only thing to change per run
 
 # %%
 # --- Install dependencies ---
