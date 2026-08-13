390670e030fd6d03a6bb8685924b82ad37d0016c
Enes Demir <123deli4567@gmail.com>
2026-08-13 20:19:34 +0300

    tryin lower lr

--- colab/run_experiment_colab.py
+++ colab/run_experiment_colab.py
@@ -15,7 +15,7 @@
 # GatedRepoError regardless of the token being valid.
 
 # %%
-CONFIG_PATH = "configs/experiments/exp008d_native_berturk_mse_recipe_30pct.yaml"  # <- only thing to change per run
+CONFIG_PATH = "configs/experiments/exp008e_native_berturk_mse_recipe_30pct_lower_lr.yaml"  # <- only thing to change per run
 
 # %%
 # --- Install dependencies ---
