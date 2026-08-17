ef2ae4504125d1683d6b8420663dd64d491c95ca
Enes Demir <123deli4567@gmail.com>
2026-08-17 13:15:37 +0300

    lowering lr

--- colab/run_experiment_colab.py
+++ colab/run_experiment_colab.py
@@ -15,7 +15,7 @@
 # GatedRepoError regardless of the token being valid.
 
 # %%
-CONFIG_PATH = "configs/experiments/exp009c_compositional_root_suffix_30pct_mse.yaml"  # <- only thing to change per run
+CONFIG_PATH = "configs/experiments/exp009d_compositional_root_suffix_30pct_lower_lr.yaml"  # <- only thing to change per run
 
 # %%
 # --- Install dependencies ---
