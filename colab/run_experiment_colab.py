3160814a76c7322ab2a6540959601da5c54b8b0c
Enes Demir <123deli4567@gmail.com>
2026-08-18 12:21:24 +0300

    first full run

--- colab/run_experiment_colab.py
+++ colab/run_experiment_colab.py
@@ -15,7 +15,7 @@
 # GatedRepoError regardless of the token being valid.
 
 # %%
-CONFIG_PATH = "configs/experiments/exp009i_compositional_root_suffix_8layer_body.yaml"  # <- only thing to change per run
+CONFIG_PATH = "configs/experiments/exp009j_compositional_root_suffix_full_data.yaml"  # <- only thing to change per run
 
 # %%
 # --- Install dependencies ---
